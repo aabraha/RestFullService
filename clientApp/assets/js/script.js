a6b2fe33bfd08ec80d2189c4e6683c6a48682a5a
Assefa <assefamit@gamil.com>
2020-06-27 13:56:05 -0500

    Rest release 00

---
+++ clientApp/assets/js/script.js
@@ -0,0 +1,17 @@
+ $(document).ready(function() {
+
+    $('#empolyees').DataTable({
+        "ajax": {
+            //"url": "data/test.json", //put your RESTfull controll End-point here 
+            "url": "http://localhost:9090/api/employees", 
+            "dataSrc": "", //this is important since ajax response expects data:[{}]
+        },
+        "columns": [
+                    {"data": "id"}, 
+                    {"data": "firstName"}, 
+                    {"data": "lastName"}, 
+                    {"data": "email"}
+                ]
+    });
+
+ });
